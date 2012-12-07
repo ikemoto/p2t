146775aec6399ede976a05b121f5f240441db071
ikemoto <ikemoto@ikemoto-HP>
2012-12-07 16:32:29 +0900

    全面的に修正

--- src/com/ikehiroki/p2t/activity/PhotoActivity.java
+++ src/com/ikehiroki/p2t/activity/PhotoActivity.java
@@ -1,102 +1,55 @@
 package com.ikehiroki.p2t.activity;
 
-import java.io.FileOutputStream;
 
 import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
-import android.view.Menu;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class PhotoActivity extends Activity {
 
-	protected Camera camera;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_photo);
-		SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surface_view);
-        SurfaceHolder holder = surfaceView.getHolder();
-        holder.addCallback(surfaceListener);
-
-	
-	}
-	
-	private SurfaceHolder.Callback surfaceListener = new SurfaceHolder.Callback() {
-
-		@Override
-		public void surfaceChanged(SurfaceHolder holder, int format, int width,
-				int height) {
-			camera.stopPreview();
-	        Camera.Parameters params = camera.getParameters();
-	        params.setPreviewFormat(format);
-	        params.setPreviewSize(width, height);
-	        camera.setParameters(params);
-	        camera.startPreview();
-		}
-
-		@Override
-		public void surfaceCreated(SurfaceHolder holder) {
-	        camera = Camera.open();
-	        try {
-	        	camera.setPreviewDisplay(holder);
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-		}
-
-		@Override
-		public void surfaceDestroyed(SurfaceHolder holder) {
-			if (camera != null) {
-				camera.stopPreview();
-				camera.release();
-				camera = null;
-	        }
-		}
-
-	};
-	
-	// シャッターが押されたときに呼ばれるコールバック
-	private Camera.ShutterCallback mShutterListener =
-	    new Camera.ShutterCallback() {
-	        public void onShutter() {
-	            // TODO Auto-generated method stub
-	        }
-	    };
-	 
-	// JPEGイメージ生成後に呼ばれるコールバック
-	private Camera.PictureCallback mPictureListener =
-	    new Camera.PictureCallback() {
-	        public void onPictureTaken(byte[] data, Camera camera) {
-	            // SDカードにJPEGデータを保存する
-	            if (data != null) {
-	                FileOutputStream stream = null;
-	                try {
-	                	stream = new FileOutputStream("/sdcard/camera_test.jpg");
-	                	stream.write(data);
-	                	stream.close();
-	                } catch (Exception e) {
-	                    e.printStackTrace();
-	                }
-	 
-	                camera.startPreview();
-	            }
-	        }
-	    };
-	 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-	    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-	        if (camera != null) {
-	            camera.takePicture(mShutterListener, null, mPictureListener);
-	        }
-	    }
-	    return true;
-	}
-
+	private CameraPreview mPreview;
+    Camera mCamera;
+    int numberOfCameras;
+    int cameraCurrentlyLocked;
+    int defaultCameraId;
+     
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);        
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mPreview = new CameraPreview(this);        
+        setContentView(mPreview);
+         
+        numberOfCameras = Camera.getNumberOfCameras();
+        CameraInfo cameraInfo = new CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                defaultCameraId = i;
+            }
+        }
+    }
+     
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCamera = Camera.open();
+        cameraCurrentlyLocked = defaultCameraId;
+        mPreview.setCamera(mCamera);
+    }
+     
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCamera != null) {
+            mPreview.setCamera(null); 
+            mCamera.release();
+            mCamera = null;
+        }   
+    } 
 
 }
